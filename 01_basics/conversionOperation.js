c864bc07cd30bb9cbb9d3fabb73a4966495a1d33
vaibhavmca191 <129191924+vaibhavmca191@users.noreply.github.com>
2024-11-18 19:35:15 +0000

    done

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score=true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false=>0
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1 => true;0=>false
+//""=>false
+// "vaibhav"=>true
+
+let someNumber=44
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=144553
+let accountEmail="vaibhav@google.com"
+var accountPassword ="12345"
+accountCity="Pune"
+
+accountEmail="vt@v.com"
+accountPassword="222213121"
+accountCity="parbhani"
+let accountstate;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
