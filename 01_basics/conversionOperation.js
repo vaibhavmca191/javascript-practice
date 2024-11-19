adcdc47681826eb9300ff9bc1ed968be0ef3c9b3
vaibhavmca191 <129191924+vaibhavmca191@users.noreply.github.com>
2024-11-19 09:33:44 +0000

    change in locations of files

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -25,3 +25,26 @@ let someNumber=44
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ***********************OPERATIONS****************************************
+let value=2
+let negvalue=-value
+//console.log(negvalue);
+console.log(2+2);
+console.log(2*2);
+
+let str1="hello"
+let str2="vaibhav"
+
+let str3=str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log((3 + 4) * 5 % 3)
+console.log(+true);
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,35 @@
+// primitive -
+
+// 7 types :String,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+// const score=100
+// const scoreValue=100.3
+
+// const isLoggedIn=false
+// const outsideTemp=null
+// let userEmail;
+
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+// console.log(id==anotherId);
+
+const bigNumber =34522234533n
+
+const heros=["shaktiman","nagarjun","doga"];
+let myobj={
+    name:"vaibhav",
+    age:23,
+}
+
+const myFunction=function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof bigNumber);
+console.log(typeof myFunction);
+
+
+
+// Reference(Non-Primitive)
+// Arrays,Objects,Functions
