af5fa117ab48eae3c7d96e3edc7ed9a166d10860
vaibhavmca191 <129191924+vaibhavmca191@users.noreply.github.com>
2024-11-28 13:32:56 +0000

    commit change

---
+++ 01_basics/arrays.js
@@ -0,0 +1,54 @@
+//array
+
+const myArr=[0,1,2,3,4]
+const myHeros=["batman","spidi","ironman"]
+
+const myArr2=new Array(1,2,3,4)
+//console.log(myArr[3]);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.includes(4));
+// console.log(myArr);
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+// console.log("A", myArr);
+
+// const myn1 = myArr.slice(1,3)
+
+// console.log(myn1);
+// console.log("B", myArr);
+
+// const myn2 = myArr.splice(1, 3)
+// console.log("c",myArr);
+
+// console.log(myn2);
+// console.log(myArr);
+
+// const marvel_heros= ["thor","hulk","captain America"]
+// const dc_heros = ["superman","flash","batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+
+
+
+
+
+
+
+
+
