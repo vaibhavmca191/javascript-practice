6b5a5b0e140c2efbc74bbfa5f4cefd3799a38d9f
vaibhavmca191 <129191924+vaibhavmca191@users.noreply.github.com>
2024-12-01 14:47:46 +0000

    change it

--- 01_basics/functions.js
+++ 01_basics/functions.js
@@ -37,4 +37,38 @@ function loginUserMessage(username = "vaibahv"){
 }
 
 
-console.log(loginUserMessage("vaibhav"));
+// console.log(loginUserMessage("vaibhav"));
+
+function calculateCarPrice(val1, val2, ...num1){
+    return num1
+
+}
+// console.log(calculateCarPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "vaibhav",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and 
+        price is ${anyobject.price}`);
+    }
+    
+// handleObject(user)
+handleObject({
+    username: "vaibhav",
+    price: 499
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]));
+
+
+
