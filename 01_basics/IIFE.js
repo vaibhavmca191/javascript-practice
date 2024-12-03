6b6774552067e4ee2ade65759dbaed98f020f6ac
vaibhavmca191 <129191924+vaibhavmca191@users.noreply.github.com>
2024-12-03 19:11:29 +0000

    IFFE changes

---
+++ 01_basics/IIFE.js
@@ -0,0 +1,20 @@
+//Immediately Invoked Function Expressions (IIFE)
+
+(function chai() {
+    console.log(`DB CONNECTED`);
+    
+    
+})()
+
+
+(function chai() {
+    console.log(`DB CONNECTED`);
+    
+    
+})();
+
+( (name) => {
+    
+    console.log(`DB CONNECTED Two ${name}`);
+    
+})('vaibahv')
