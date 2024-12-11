0f7bc4a499b73f3a5691cea2458367183fd7bae6
vaibhavmca191 <129191924+vaibhavmca191@users.noreply.github.com>
2024-12-11 11:12:39 +0000

    commit change

---
+++ 01_basics/Control_Flow.js
@@ -0,0 +1,56 @@
+const isUserloggedIn = true
+if( 2 == "2"){
+    console.log("executed");
+    
+}
+
+// const temprature=41
+// if(temprature<50)
+// {
+//     console.log("less than 50");
+    
+// }else{
+//     console.log("temprature is greather than 50");
+
+// }
+
+const score = 200
+
+// if(score>100){
+//     const power = "fly"
+//     console.log(`User powe:${power}`);
+    
+// }
+// console.log(`User powe:${power}`);
+
+// const balance = 1000
+
+// //if(balance>500) console.log("test"),console.log("test2");
+
+// if(balance<500){
+//     console.log("less than");
+
+// }else if(balance<750){
+//     console.log("less than 750");
+    
+// }else if(balance < 900){
+//     console.log("less than 750");
+    
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+const UserLoggedIn = true
+const debitCard = true
+const UserLoggedInFromGoogle = false
+const UserLoggedInFromEmail = true
+if(UserLoggedIn && debitCard && 2==2){
+    console.log("Allows to buy course");
+    
+}
+
+if(UserLoggedInFromGoogle || UserLoggedInFromEmail){
+    console.log("User logged in");
+    
+}
