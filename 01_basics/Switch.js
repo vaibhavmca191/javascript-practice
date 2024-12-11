1fdc36e39d3a942647094936696653d13f392168
vaibhavmca191 <129191924+vaibhavmca191@users.noreply.github.com>
2024-12-11 11:15:48 +0000

    yuppp

---
+++ 01_basics/Switch.js
@@ -0,0 +1,19 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("january"); 
+        break;
+        
+
+    default:
+        break;
+}
