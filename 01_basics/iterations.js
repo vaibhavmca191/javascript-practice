c23cbe64e6282722843ff53dafcbeab018dc0a0b
vaibhavmca191 <129191924+vaibhavmca191@users.noreply.github.com>
2024-12-23 20:07:11 +0000

    done

--- 01_basics/iterations.js
+++ 01_basics/iterations.js
@@ -61,12 +61,43 @@ for (let index = 0; index < myArray.length; index++) {
 
 for (let index = 1; index <= 20; index++) {
     if(index == 5){
-        console.log(`Detected 5`);
+       // console.log(`Detected 5`);
        continue
         
     }
-    console.log(`value of i is ${index}`);
+    //console.log(`value of i is ${index}`);
     
    
     
 }
+
+
+
+//while and do-while loops
+
+let index = 0
+while (index <= 10) {
+    //console.log(`Value of index is ${index}`);
+    index = index + 2
+
+    
+}
+
+let myArray1 = ['ironman' , "batman", "Hulk"]
+
+let arr = 0
+while (arr < myArray1.length){
+    //console.log(`value is ${myArray1[arr]}`);
+    arr = arr + 1
+    
+}
+
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+        score++
+    
+} while (score <= 10);
+
